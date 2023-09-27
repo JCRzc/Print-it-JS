032b936f5b312dc3d4ce95e27fd409a64db02edc
JC R <christophe.rezac@laposte.net>
2023-09-27 13:03:54 +0200

    Final commit

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,58 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-let arrow_left = document.querySelector(".arrow_left")
-arrow_left.addEventListener("click", () => {
-	console.log("J'ai cliqué sur la flèche gauche")
-}) 
+// Récupérer les éléments HTML 
+let arrowLeft = document.querySelector(".arrow_left")
+let arrowRight = document.querySelector(".arrow_right")
+let dot = document.querySelectorAll(".dot")
+let slideImage = document.querySelector(".banner-img")
+let tagline = document.getElementById("tagline")
 
-let arrow_right = document.querySelector(".arrow_right")
-arrow_right.addEventListener("click", () => {
-	console.log("J'ai cliqué sur la flèche droite")
+let currentIndex = 0; 
+
+// Fonction pour mettre à jour la diapositive actuelle 
+function updateSlide(index) {
+	let slide = slides[index]
+	slideImage.src = slide.image
+	tagline.innerHTML = slide.tagLine
+
+// Mettre à jour les bullet points 
+	dot.forEach((bullet, i) => {
+		if (i === index) {
+			bullet.classList.add("dot_selected")
+		} else {
+			bullet.classList.remove("dot_selected")
+		}
+	})
+}
+
+// Écouteur de clic pour la flèche de droite 
+arrowRight.addEventListener("click", () => {
+	currentIndex = (currentIndex + 1) % slides.length;
+	updateSlide (currentIndex)
 })
+
+// Écouteur de clic pour la flèche de gauche
+arrowLeft.addEventListener("click", () => {
+	currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+	updateSlide (currentIndex)
+})
+
+// Initialiser la première diapositive
+updateSlide(currentIndex)
